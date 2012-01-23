cd3772834c5a2d6695c179fb2b77c87f766372f5
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-01-23 17:27:08 +0100

    Ajout gestion Irrlicht

--- main.cpp
+++ main.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
 
+#include<irrlicht/irrlicht.h>
+
 #include "niveau.hpp"
 #include "case.hpp"
 #include "objet.hpp"
 
 using namespace std;
+using namespace irr;
 
 int main(int argc, char *argv[])
 {
+					     // api graphique : opengl, dimension fenetre : 800x600, bytes par pixel, 
+					     // fullscreen, stencilbuffer, vsync, receiver
+	IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(800, 600), 32, false, true, false, 0);
+	video::IVideoDriver *driver = device->getVideoDriver();
+	scene::ISceneManager *sceneManager = device->getSceneManager();
+
+	sceneManager->addCameraSceneNode(0, core::vector3df(0, 0, 0), core::vector3df(5, 0, 0));
+
+
+	while(device->run())
+	{
+		driver->beginScene(true, true, video::SColor(255, 255, 255, 255));
+		sceneManager->drawAll();
+		driver->endScene();
+	}
+
 	// Creuser un mur
 	// Ajouter un objet
+
+	device->drop();
 	return 0;
 }
