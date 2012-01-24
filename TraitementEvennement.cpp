d87a2146f1568b57e7d85630c91b2307ac625159
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-01-25 00:00:11 +0100

    Ajout d'une classe personnalisee de traitement des evennements claviers et souris

---
+++ TraitementEvennement.cpp
@@ -0,0 +1,68 @@
+#include<irrlicht/irrlicht.h>
+
+#include "TraitementEvennement.hpp"
+
+using namespace irr;
+
+TraitementEvennement::TraitementEvennement()
+{
+	for(u32 i = 0; i < KEY_KEY_CODES_COUNT; i++)
+		m_KeyIsDown[i] = false;
+	m_IsShiftDown = false;
+	m_IsCtrlDown = false;
+
+}
+
+TraitementEvennement::~TraitementEvennement()
+{
+}
+
+bool TraitementEvennement::OnEvent(const SEvent &event)
+{
+	switch(event.EventType)
+	{
+		case EET_KEY_INPUT_EVENT :
+			m_KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+			m_IsCtrlDown = event.KeyInput.Control;
+			m_IsShiftDown = event.KeyInput.Shift;
+			break;
+		case EET_MOUSE_INPUT_EVENT :
+			switch(event.MouseInput.Event)
+			{
+				case EMIE_LMOUSE_PRESSED_DOWN :
+					MouseState.LeftButtonDown = true;
+					break;
+				case EMIE_LMOUSE_LEFT_UP:
+					MouseState.LeftButtonDown = false;
+					break;
+				case EMIE_RMOUSE_PRESSED_DOWN : 
+					MouseState.RightButtonDown = true;
+					break;
+				case EMIE_RMOUSE_LEFT_UP : 
+					MouseState.RightButtonDown = false;
+					break;
+				case EMIE_MOUSE_MOVED :
+					MouseState.Position.X = event.MouseInput.X;
+					MouseState.Position.Y = event.MouseInput.Y;
+					break;
+				default :
+					/*
+					 * Clic milieu,
+					 * Pour les trois : 
+					 *  * double clics
+					 *  * triple clics
+					 * mouvement molette
+					 *
+					 */
+					break;
+			}
+			break; // Fin du MOUSE_INPUT_EVENT
+		default : break;
+	}
+		
+		
+	return false;
+}
+
+bool TraitementEvennement::IsKeyDown(EKEY_CODE keyCode) const
+{ return m_KeyIsDown[keyCode]; }
---
+++ TraitementEvennement.hpp
@@ -0,0 +1,32 @@
+#ifndef HPP_EVENT
+#define HPP_EVENT
+
+#include<irrlicht/irrlicht.h>
+
+class TraitementEvennement : public irr::IEventReceiver
+{
+	public :
+		struct SMouseState
+		{
+			irr::core::position2di Position;
+			bool LeftButtonDown;
+			bool RightButtonDown;
+			SMouseState() : LeftButtonDown(false), RightButtonDown(false) { }
+		} MouseState;
+		
+		TraitementEvennement();
+		~TraitementEvennement();
+
+		virtual bool OnEvent(const irr::SEvent &event);
+		virtual bool IsKeyDown(irr::EKEY_CODE keyCode) const;
+
+		const SMouseState & GetMouseState() const
+		{ return MouseState; }
+	private :
+		bool m_KeyIsDown[irr::KEY_KEY_CODES_COUNT];
+		bool m_IsShiftDown;
+		bool m_IsCtrlDown;
+};
+
+
+#endif
