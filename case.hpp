5339096b9772473e8b2cbc263f64434e72edb419
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2012-02-10 17:01:58 +0100

    Correction d'accès mémoire

--- case.hpp
+++ case.hpp
@@ -23,11 +23,11 @@ class Case
 		
 		// Accesseur
 		TypeCase getTypeDeLaCase() { return m_TypeDeLaCase; }
-		TypeObjet getTypeObjet() { return m_Objet->getTypeObjet(); }
-		bool getObjetActif() { return m_Objet->getActif(); }
-		void setObjetActif(bool value) { m_Objet->setActif(value); }
-		bool getObjetActivite() { return m_Objet->getActiviteEnCours(); }
-		void setObjetActivite(bool value) { m_Objet->setActiviteEnCours(value) ;}
+		TypeObjet getTypeObjet() { return (m_Objet ? m_Objet->getTypeObjet() : NULL_OBJET ); }
+		bool getObjetActif() { return (m_Objet ? m_Objet->getActif() : false); }
+		void setObjetActif(bool value) { if(m_Objet) { m_Objet->setActif(value); } }
+		bool getObjetActivite() { return (m_Objet ? m_Objet->getActiviteEnCours() : false); }
+		void setObjetActivite(bool value) { if(m_Objet) { m_Objet->setActiviteEnCours(value) ; } }
 
 		void setSmooth() { m_IsSmooth = true; }
 		bool getIsSmooth() { return m_IsSmooth; }
